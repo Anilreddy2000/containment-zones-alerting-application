32d488ca9e410c83b589a77afd1b3a537b7471fa
Anilreddy2000 <anilkumarreddy10062000@gmail.com>
2023-01-11 20:06:30 +0530

    file

---
+++ app/src/main/java/com/example/geofence/Registration.java
@@ -0,0 +1,129 @@
+package com.example.geofence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Registration extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    EditText address;
+    EditText mobile;
+    EditText name;
+    TextView submit,login;
+    String emailid;
+    String pass,mobileNum,addr,fname;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+        name=(EditText)findViewById(R.id.name);
+        address=(EditText)findViewById(R.id.address);
+        mobile=(EditText)findViewById(R.id.mobile);
+
+        submit=(TextView) findViewById(R.id.submit);
+        login=(TextView) findViewById(R.id.login);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailid=email.getText().toString();
+                pass=password.getText().toString();
+                mobileNum=mobile.getText().toString();
+                addr=address.getText().toString();
+                fname=name.getText().toString();
+                if(!emailid.isEmpty() || !pass.isEmpty()||!mobileNum.isEmpty() || !addr.isEmpty()||!fname.isEmpty()) {
+                    register reg=new register();
+                    reg.execute();
+                }
+                else{
+                    email.setError("Please Enter Email Id");
+                    password.setError("Please Enter Password");
+
+                }
+
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Registration.this, Login.class);
+                startActivity(in);
+            }
+        });
+    }
+
+    class register extends AsyncTask<Void,Void,String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                //Initializing the URL
+                URL url = new URL("http://18.207.120.122:3002/signup");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setInstanceFollowRedirects(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("charset", "utf-8");
+                connection.setUseCaches (false);
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream ());
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("email", emailid);
+                jsonParam.put("password1", pass);
+                jsonParam.put("address", addr);
+                jsonParam.put("mobile", mobileNum);
+                jsonParam.put("name", fname);
+                wr.writeBytes(jsonParam.toString());
+                wr.flush();
+                wr.close();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                System.out.println(" bufferedreader response :" + bufferedReader);
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line);
+
+
+                }
+                bufferedReader.close();
+                Log.e("res", stringBuilder.toString());
+                return stringBuilder.toString();
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.e("Output--1",""+s);
+
+        }
+    }
+}
